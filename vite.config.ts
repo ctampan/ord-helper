2ac5dbb7083e6f09e7b9fa921e53c2ca4d0d0b15
Akhmad Ramadhan Yamin <akhmadr.yamin@gmail.com>
2025-11-22 10:16:29 +0700

    Adjust deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ord-helper/',
+  base: process.env.NODE_ENV === 'production' ? '/ord-helper/' : '/',
 })
